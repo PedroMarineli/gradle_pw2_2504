b363daa41f28e445092350a762182b57d5019abf
PedroMarineli <phmo.1606@gmail.com>
2022-04-27 17:10:45 +0000

    kjjjj

--- src/main/java/gradlegs/App.java
+++ src/main/java/gradlegs/App.java
@@ -5,7 +5,13 @@ public class App {
         Genero g = new Genero();
         g.id = 1;
         g.nome = "Aventura";
-        
+        Genero g2 = new Genero();
+        g2.id = 2;
+        g2.nome = "Ação";
+        Genero g3 = g;
+        g.nome = "Técnico";
+
         System.out.println("Olá Gradle");
+        System.out.println(g.nome);
     }
 }
