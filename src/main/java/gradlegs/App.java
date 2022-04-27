74bbe3848315c73daac75d3cba94785cdf97cf26
PedroMarineli <phmo.1606@gmail.com>
2022-04-27 17:03:47 +0000

    df

--- src/main/java/gradlegs/App.java
+++ src/main/java/gradlegs/App.java
@@ -2,6 +2,10 @@ package gradlegs;
 
 public class App {
     public static void main(String[] args) {
+        Genero g = new Genero();
+        g.id = 1;
+        g.nome = "Aventura";
+        
         System.out.println("Olá Gradle");
     }
 }
---
+++ src/main/java/gradlegs/Genero.java
@@ -0,0 +1,8 @@
+package gradlegs;
+
+public class Genero {
+    public int id;
+    public String nome;
+
+    
+}
