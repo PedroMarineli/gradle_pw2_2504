0c368fb6424ac1a9ab8c06eba51834483b9325b5
PedroMarineli <phmo.1606@gmail.com>
2022-04-27 17:23:44 +0000

    çlpp

--- src/main/java/gradlegs/Genero.java
+++ src/main/java/gradlegs/Genero.java
@@ -4,5 +4,11 @@ public class Genero {
     public int id;
     public String nome;
 
-    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id =id;
+    }
 }
